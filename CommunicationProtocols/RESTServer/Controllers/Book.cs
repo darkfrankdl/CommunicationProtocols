a02e7d0f9fa727c1980fac8d4c9fb24d960512c9
darkfrankdl <businessLundt@gmail.com>
2024-10-10 21:53:46 +0200

    Made Server - Get method

---
+++ CommunicationProtocols/RESTServer/Controllers/Book.cs
@@ -0,0 +1,7 @@
+﻿namespace RESTServer.Controllers
+{
+    public class Book
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ CommunicationProtocols/RESTServer/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RESTServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        List<Book> _books;
+        public BookController() 
+        {
+            _books = new List<Book>() { new Book() { Id = 1 } };
+        }
+
+        [HttpGet]
+        public ActionResult<Book> Get(int id)
+        {
+            Book book = _books[id];
+            return book;
+        }
+    }
+}
